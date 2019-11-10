e3ea332af1814b194864838f6a89485db84efda3
gizmond <lukas-yu@hotmail.com>
2019-11-10 23:19:49 +0100

    Add bpe script

--- bpe.sh
+++ bpe.sh
@@ -1,10 +1,10 @@
 sh preprocess_experiment_data.sh
 
-python train.py --data experiment/prepared_data --cuda True
+python train.py --data experiment/prepared_data --cuda True --save-dir bpe
 
-python translate.py --data experiment/prepared_data --cuda True
+python translate.py --data experiment/prepared_data --cuda True --checkpoint-path bpe/checkpoint_best.pt --output bpe/test.txt
 
-sh postprocess.sh model_translations.txt model_translations.postprocessed.txt en
+sh postprocess.sh bpe/test.txt bpe/test.txt en
 
 echo "BPE model" >> output.txt
-cat model_translations.postprocessed.txt | sacrebleu experiment/raw_data/test.en >> output.txt
+cat bpe/test.txt | sacrebleu experiment/raw_data/test.en >> output.txt
